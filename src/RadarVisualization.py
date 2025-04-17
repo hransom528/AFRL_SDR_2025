bea691f90cf57a1ef735185196281a6fe6f8c2e4
Harris Ransom <hransom528@gmail.com>
2025-04-16 21:26:08 -0400

    Started button implementation

--- src/RadarVisualization.py
+++ src/RadarVisualization.py
@@ -5,6 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Button
 
 # Convert polar coords to rectangular coords
 def rect2polar(coord, degrees=True):
@@ -55,6 +56,29 @@ def radarUpdate(frame):
 	ax.scatter(theta, r)
 	fig.canvas.draw()
 
+# Buttons to switch between different algorithms
+class Switcher():
+	# Algortihm selector
+	algorithms = ["triangulation", "tdoa", "ml_nobaseband", "ml_baseband"]
+	alg = 0
+
+	# TODO: Implement different data feeds from other algorithms
+	# Triangulation button callback
+	def triangulation(self, event):
+		alg = 0
+
+	# TDoA button callback
+	def tdoa(self, event):
+		alg = 1
+
+	# ML (No Baseband) button callback
+	def ml_nobaseband(self, event):
+		alg = 2
+
+	# ML w/Baseband button callback
+	def ml_baseband(self, event):
+		alg = 3
+
 # MAIN
 if __name__ == "__main__":
 	# Creates the figure and axes object
@@ -65,6 +89,23 @@ if __name__ == "__main__":
 	r = 2
 	theta = np.deg2rad(60)
 	#staticRadarPlot((r, theta))
+
+	# Adds buttons
+	ax_triangulation = fig.add_axes([0.55, 0.01, 0.1, 0.075])
+	ax_tdoa 		 = fig.add_axes([0.66, 0.01, 0.1, 0.075])
+	ax_nobaseband 	 = fig.add_axes([0.77, 0.01, 0.1, 0.075])
+	ax_baseband 	 = fig.add_axes([0.88, 0.01, 0.1, 0.075])
+
+	callback = Switcher()
+	btn_triangulation = Button(ax_triangulation, 'Triangulation')
+	btn_tdoa = Button(ax_tdoa, "TDoA")
+	btn_nobaseband = Button(ax_nobaseband, "ML (No Baseband)")
+	btn_baseband = Button(ax_baseband, "ML w/Baseband")
+
+	btn_triangulation.on_clicked(callback.triangulation)
+	btn_tdoa.on_clicked(callback.tdoa)
+	btn_nobaseband.on_clicked(callback.ml_nobaseband)
+	btn_baseband.on_clicked(callback.ml_baseband)
 
 	# Creates animation object
 	anim = FuncAnimation(fig, radarUpdate)
